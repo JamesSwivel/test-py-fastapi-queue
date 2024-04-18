0699686266c8cfdb9d4d28f53dba5a8dd5f16770
jw-redpanda <jameswongkb@gmail.com>
2024-04-19 00:10:43 +0800

    test pdf2image blocking

--- src/lib/api/simple.py
+++ src/lib/api/simple.py
@@ -1,13 +1,35 @@
+import os
 from typing import Optional
 from fastapi import FastAPI, Body, HTTPException, File, UploadFile, Form, Depends
 from pydantic import BaseModel, Field, validator
+import pdf2image
 
 import util as U
 from .util import throwHttpPrefix
+from .worker.types import QueueJobResult
 
 
 class SimpleRes(BaseModel):
     data: str
+
+
+def onJobPdf2image(jobId: str, jobResult: QueueJobResult):
+    funcName = onJobPdf2image.__name__
+    prefix = f"{funcName}[{jobId}]"
+    try:
+        pdfPath = f"./data/regal-17pages.pdf"
+        U.logD(f"{prefix} converting {pdfPath}")
+        pages = pdf2image.convert_from_path(pdfPath, thread_count=4)
+        if pages is not None and len(pages) > 0:
+            basedDir = f"./out/pdf2image/{jobId}"
+            os.makedirs(basedDir)
+            for idx, page in enumerate(pages):
+                page.save(f"{basedDir}/image-{idx:0>2}.png")
+        jobResult["data"] = f"pdf2image finished ({U.epochMs()}), nPages={len(pages)}"
+        U.logD(f"{prefix} finished")
+    except Exception as e:
+        U.logPrefixE(prefix, e)
+        U.throwPrefix(prefix, e)
 
 
 def initEndpoints(app: FastAPI):
@@ -16,8 +38,13 @@ def initEndpoints(app: FastAPI):
     @app.get("/hello", response_model=SimpleRes)
     @app.post("/hello", response_model=SimpleRes)
     async def hello(data: str = Body(..., embed=True)):
-        res = {"data": f"received data={data}!"}
-        return res
+        funcName = hello.__name__
+        prefix = funcName
+        try:
+            res = {"data2": f"received data={data}!", "password": "secret"}
+            return res
+        except Exception as e:
+            throwHttpPrefix(prefix, e)
 
     @app.post("/getInfo", response_model=SimpleRes)
     async def get_info(data: str = Body(..., embed=True)):
@@ -29,5 +56,25 @@ def initEndpoints(app: FastAPI):
 
             res = {"data": f"received data={data}!"}
             return res
+        except Exception as e:
+            throwHttpPrefix(prefix, e)
+
+    @app.post("/pdf2image", response_model=SimpleRes)
+    async def postPdf2image(data: str = Body(..., embed=True)):
+        funcName = postPdf2image.__name__
+        prefix = funcName
+        try:
+            jobId = U.uuid()
+            jobResult: QueueJobResult = {
+                "errCode": "",
+                "err": "",
+                "data": "",
+                "workerName": "test",
+                "dequeueElapsedMs": 0,
+                "processElapsedMs": 0,
+                "totalElapsedMs": 0,
+            }
+            onJobPdf2image(jobId, jobResult)
+            return jobResult
         except Exception as e:
             throwHttpPrefix(prefix, e)
