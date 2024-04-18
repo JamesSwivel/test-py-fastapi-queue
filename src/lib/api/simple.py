e7781b75100ca39342cf03fe702b55dd524fc337
jw-redpanda <jameswongkb@gmail.com>
2024-04-18 22:28:40 +0800

    define response_model, handle correct exception

--- src/lib/api/simple.py
+++ src/lib/api/simple.py
@@ -1,23 +1,33 @@
+from typing import Optional
 from fastapi import FastAPI, Body, HTTPException, File, UploadFile, Form, Depends
+from pydantic import BaseModel, Field, validator
 
 import util as U
+from .util import throwHttpPrefix
+
+
+class SimpleRes(BaseModel):
+    data: str
 
 
 def initEndpoints(app: FastAPI):
     U.logD(f"{initEndpoints.__name__}[{__file__.split('/')[-1]}] loading...")
 
-    @app.get("/hello")
-    @app.post("/hello")
+    @app.get("/hello", response_model=SimpleRes)
+    @app.post("/hello", response_model=SimpleRes)
     async def hello(data: str = Body(..., embed=True)):
-        return {"data": f"received data={data}!"}
+        res = {"data": f"received data={data}!"}
+        return res
 
-    @app.post("/getInfo")
+    @app.post("/getInfo", response_model=SimpleRes)
     async def get_info(data: str = Body(..., embed=True)):
         funcName = get_info.__name__
         prefix = funcName
         try:
             if data != "hello":
                 raise Exception("data is not hello")
-            return {"data": f"received data={data}!"}
+
+            res = {"data": f"received data={data}!"}
+            return res
         except Exception as e:
-            U.logPrefixE(prefix, e)
+            throwHttpPrefix(prefix, e)
