e8bae557cf257592578b03c586521d6a12416abb
JamesSwivel <james.wong@swivelsoftware.com>
2024-04-19 12:23:34 +0800

    typedDict

--- src/lib/api/nestedPayload.py
+++ src/lib/api/nestedPayload.py
@@ -11,6 +11,7 @@ from typing import (
     Annotated,
     Optional,
     NotRequired,
+    cast,
 )
 from fastapi import FastAPI, Body, HTTPException, File, UploadFile, Form, Depends
 from pydantic import BaseModel, Field, validator
@@ -19,39 +20,78 @@ import util as U
 from util.fastApi import throwHttpPrefix
 
 
-class Address(BaseModel):
+###########################################################################
+### CompanyResponseBM and CompanyResponse
+###########################################################################
+###-----------------------------------------------
+class AddressBM(BaseModel):
     street: str
     postalCode: Optional[str]
 
 
-class Person(BaseModel):
+class Address(TypedDict):
+    street: str
+    postalCode: Optional[str]
+
+
+###-----------------------------------------------
+class PersonBM(BaseModel):
+    name: str
+    age: int
+    address: AddressBM
+
+
+class Person(TypedDict):
     name: str
     age: int
     address: Address
 
 
-class Department(BaseModel):
+###-----------------------------------------------
+class DepartmentBM(BaseModel):
+    name: str
+    region: str
+    persons: List[PersonBM]
+
+
+class Department(TypedDict):
     name: str
     region: str
     persons: List[Person]
 
 
-class Company(BaseModel):
+###-----------------------------------------------
+class CompanyBM(BaseModel):
     name: str
     extra1: Optional[str]
     extra2: Optional[str] = Field(None, description="payload extra2")
+    departments: Dict[str, DepartmentBM]
+
+
+class Company(TypedDict):
+    name: str
+    extra1: Optional[str]
+    extra2: NotRequired[Optional[str]]
     departments: Dict[str, Department]
 
 
+###-----------------------------------------------
+class CompanyResponseBM(BaseModel):
+    data: CompanyBM
+
+
 class CompanyResponse(BaseModel):
-    data: Company
+    data: CompanyBM
+
+
+###########################################################################
 
 
 def initEndpoints(app: FastAPI):
     U.logD(f"{initEndpoints.__name__}[{__file__.split('/')[-1]}] loading...")
 
-    @app.post("/nestedPayload", response_model=CompanyResponse)
-    async def nestedPayload(data: Company):
+    @app.post("/nestedPayload", response_model=CompanyResponseBM)
+    async def nestedPayload(data: CompanyBM):
         funcName = nestedPayload.__name__
         prefix = funcName
         try:
@@ -64,9 +104,10 @@ def initEndpoints(app: FastAPI):
         funcName = validateNestedPayload.__name__
         prefix = funcName
         try:
-            data = {
+            data: Company = {
                 "name": "Swivel Software Limited",
                 "extra1": None,
+                "extra2": None,
                 "departments": {
                     "IT": {
                         "name": "IT Department",
@@ -80,10 +121,10 @@ def initEndpoints(app: FastAPI):
                             {"name": "Sylvia Law", "age": 21, "address": {"street": "HKU road", "postalCode": None}},
                         ],
                     },
-                    "HR": {"name": "HR Department", "region": "KWun Tong APM", "persons": [1]},
+                    "HR": {"name": "HR Department", "region": "KWun Tong APM", "persons": []},
                 },
             }
-            company = Company(**data)
+            company = CompanyBM(**cast(dict, data))
             return company
         except Exception as e:
             throwHttpPrefix(prefix, e)
